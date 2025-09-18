6a7f8611c15aa2d77c0b316a637cedc038c889f3
Fasia-660710731 <mianaiad_f@su.ac.th>
2025-09-18 05:26:24 +0000

    Bookstore101

---
+++ bookstore-app/src/components/NotFound.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+const NotFound = () => {
+    const navigate = useNavigate();
+
+    const handleGoHome = () => {
+        navigate("/");
+    }
+    return (
+        <div>
+            <h1>404 - Not Found</h1>
+            <p>The page you are looking for does for does not exist.</p>
+        </div>
+    );
+};
+
+export default NotFound;
